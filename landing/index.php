b2919e15d2aa22a651b7b3ca13feffa22f8e8085
Oleksii Temchenko <oleksii.temchenko@gmail.com>
2025-12-17 18:15:12 +0200

    it works

--- landing/index.php
+++ landing/index.php
@@ -5,20 +5,21 @@ require_once 'bootstrap.php';
 if (empty($_REQUEST['subid']) || empty($_REQUEST['target'])) {
     throw new \Exception('Server error');
 }
+
 $subid     = $_REQUEST['subid'];
 $targetUrl = $_REQUEST['target'];
 $keitaroPostbackUrl = 'https://keistream.com/3c218da/postback';
 
 if ($subid) {
     $postData = [
-        'subid'  => $subid,
+        'subid' => $subid,
     ];
 
     $ch = curl_init($keitaroPostbackUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
-    if (!test()) {
+    if (!function_exists('test') || !test()) {
         $response = curl_exec($ch);
     }
     curl_close($ch);
@@ -27,5 +28,18 @@ if ($subid) {
     // file_put_contents("postback.log", date("c")." ".$response."\n", FILE_APPEND);
 }
 
-// Редиректим пользователя на целевой URL
-header("Location: $targetUrl");
+// Собираем все GET‑параметры, кроме target
+$queryParams = $_GET;
+unset($queryParams['target']); // чтобы не дублировать target в строке
+
+$queryString = http_build_query($queryParams);
+
+// Формируем финальный URL
+$redirectUrl = $targetUrl;
+if ($queryString) {
+    $redirectUrl .= (strpos($targetUrl, '?') === false ? '?' : '&') . $queryString;
+}
+
+// Редиректим пользователя на целевой URL с параметрами
+header("Location: $redirectUrl");
+exit;
